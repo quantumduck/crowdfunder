e808efd36f585922fdb95ffddbbaab0fc06dad1f
quantumduck <quantum.duck@hotmail.com>
2017-05-04 13:14:53 -0400

    first couple of validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,13 @@ class Project < ActiveRecord::Base
   has_many :backers, through: :rewards
   accepts_nested_attributes_for :rewards
   validates_presence_of :title, :description, :goal, :start_date, :end_date
+  validate :start_date_in_future
+
+
+private
+
+  def start_date_in_future
+    errors.add("Start Date", "must be in the future.") unless start_date > Time.now
+  end
 
 end
